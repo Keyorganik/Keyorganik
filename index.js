89f17c38e057d9110a5d0b02552eb0aa3864a9e9
orhanozkercin <orhanozkercin@yahoo.com>
2020-07-29 12:15:17 +0300

    Changed slider, added lazy loading to images. Fixed slider mobile problem.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var swiper = new Swiper('.swiper-container', {
-	slidesPerView: 3,
-	spaceBetween: 30,
-	freeMode: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
+var mySwiper = new Swiper('.swiper-container', {
+	preloadImages: false,
+
+	lazy: true,
+	fadeEffect: {
+		crossFade: true,
 	},
 	navigation: {
 		nextEl: '.swiper-button-next',
@@ -20,9 +19,13 @@ const ourMission = document.querySelector('.heading-our-mission');
 const ourVision = document.querySelector('.heading-our-vision');
 const mobileNav = document.querySelector('.mobile-navigation__nav');
 const mobileNavItems = document.querySelectorAll('.mobile-navigation__item');
-const mobileNavCheckbox = document.querySelector('.mobile-navigation__checkbox');
+const mobileNavCheckbox = document.querySelector(
+	'.mobile-navigation__checkbox'
+);
 const mobileNavButton = document.querySelector('.mobile-navigation__button');
-const mobileNavBackground = document.querySelector('.mobile-navigation__background');
+const mobileNavBackground = document.querySelector(
+	'.mobile-navigation__background'
+);
 const cardLogo1 = document.querySelector('.card__header--icon-1');
 const cardLogo2 = document.querySelector('.card__header--icon-2');
 const cardLogo3 = document.querySelector('.card__header--icon-3');
@@ -33,63 +36,54 @@ window.addEventListener('scroll', () => {
 		nav.classList.add('header__nav-scroll');
 		logo.style.width = '8rem';
 		logo.style.height = '8rem';
-		mobileNavButton.style = "top: 2.5rem"		
-		mobileNavBackground.style = "top: 2.5rem"	
+		mobileNavButton.style = 'top: 2.5rem';
+		mobileNavBackground.style = 'top: 2.5rem';
 
 		ourMission.classList.add('left-to-right');
 		ourVision.classList.add('right-to-left');
-	} 
-	else {
+	} else {
 		nav.classList.remove('header__nav-scroll');
 		logo.style.width = '12rem';
 		logo.style.height = '12rem';
-		mobileNavButton.style = "top: 4.5rem"
-		mobileNavBackground.style = "top: 4.5rem"		
+		mobileNavButton.style = 'top: 4.5rem';
+		mobileNavBackground.style = 'top: 4.5rem';
 	}
 
-	let top  = window.pageYOffset + window.innerHeight,
-      isVisible = top > document.querySelector('#differences').offsetTop;
+	let top = window.pageYOffset + window.innerHeight,
+		isVisible = top > document.querySelector('#differences').offsetTop;
 
-   if (isVisible) {
-     cardLogo1.classList.add('fade-in-1');
-     cardLogo2.classList.add('fade-in-2');
-     cardLogo3.classList.add('fade-in-3');
-     cardLogo4.classList.add('fade-in-4');
-   }
+	if (isVisible) {
+		cardLogo1.classList.add('fade-in-1');
+		cardLogo2.classList.add('fade-in-2');
+		cardLogo3.classList.add('fade-in-3');
+		cardLogo4.classList.add('fade-in-4');
+	}
 });
 
 scrollDown.addEventListener('click', () => {
 	aboutUs.scrollIntoView();
 });
 
-
 mobileNavItemsArr = Array.prototype.slice.call(mobileNavItems);
 
-mobileNavItemsArr.map((item) => item.addEventListener('click', () =>{
-	mobileNavCheckbox.checked = false;
-}
-))
+mobileNavItemsArr.map((item) =>
+	item.addEventListener('click', () => {
+		mobileNavCheckbox.checked = false;
+	})
+);
 
-mobileNavButton.addEventListener('click', () =>{
-	if(mobileNavCheckbox.checked === true){
+mobileNavButton.addEventListener('click', () => {
+	if (mobileNavCheckbox.checked === true) {
 		// mobileNavBackground.style = 'transition: "none"';
-		console.log("clicked");
+		console.log('clicked');
 		mobileNavBackground.style.transition = 'none';
-		mobileNav.style.transition = 'none'; 
+		mobileNav.style.transition = 'none';
 	}
-})
-
-
-
+});
 
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
- 
 
-	
-gulp.task('imageCompress',	() => {
-	gulp.src('./img/*')
-	.pipe(imagemin())
-	.pipe(gulp.dest('dist/images'))
-})
-
+gulp.task('imageCompress', () => {
+	gulp.src('./img/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+});
